38a3134a2709a9d02c86a1560246ab7e04bcf3ba
Anh Nguyen <ng.huyanh.gd@gmail.com>
2022-01-29 18:16:45 -0600

    add graph

---
+++ graph/src/components/Graph.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from "recharts";
+import Papa from "papaparse";
+
+export default function Graph() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        Papa.parse("./stocks/stonks.csv", {
+            download: true,
+            header: true,
+            complete: function (input) {
+                const records = input.data.reverse();
+                setData(records);
+            },
+        });
+    }, []);
+    return (
+        data && (
+            <div>
+                <LineChart
+                    width={800}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="Date" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                        type="monotone"
+                        dataKey="SPY"
+                        stroke="#8884d8"
+                        dot={false}
+                    />
+                    <Line
+                        type="monotone"
+                        dataKey="QQQ"
+                        stroke="#82ca9d"
+                        dot={false}
+                    />
+                </LineChart>
+            </div>
+        )
+    );
+}
