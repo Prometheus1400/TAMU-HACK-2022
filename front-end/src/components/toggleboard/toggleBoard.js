a1dd5c9de6907ae2b833730747b36303f543d5c7
Anh Nguyen <ng.huyanh.gd@gmail.com>
2022-01-29 23:03:57 -0600

    add pie chart

--- front-end/src/components/toggleboard/toggleBoard.js
+++ front-end/src/components/toggleboard/toggleBoard.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Switch } from "@mui/material";
 import "./toggleBoard.css";
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from "recharts";
+
+const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
+    { name: "Group E", value: 278 },
+    { name: "Group F", value: 189 },
+];
 
 function ToggleBoard({ sum, setSum }) {
     const [togArray, setToggleArray] = useState([
@@ -9,18 +19,44 @@ function ToggleBoard({ sum, setSum }) {
         false,
         false,
         false,
+        false,
+        false,
+        false,
     ]);
+    const [pieChart, setPieChart] = useState([]);
     const price = [15.49, 12.99, 14.99, 9.99, 11.99, 12.99, 9.99, 7.99];
+    const names = [
+        "Netflix",
+        "Hulu",
+        "HBO Max",
+        "Spotify",
+        "Youtube Preimum",
+        "Amazon Prime",
+        "Apple Music",
+        "Disney+",
+    ];
 
     useEffect(() => {
         let newSum = 0;
+        let newPieChart = [];
         togArray.map((item, index) => {
             if (item) {
                 newSum += price[index];
+                newPieChart = [
+                    ...newPieChart,
+                    {
+                        name: names[index],
+                        value: parseFloat(
+                            (price[index] * 12 * 1.0825).toFixed(2)
+                        ),
+                    },
+                ];
             }
         });
         console.log(newSum);
+        console.log(newPieChart);
         setSum(newSum);
+        setPieChart(newPieChart);
     }, [togArray]);
 
     const subs = [
@@ -56,6 +92,23 @@ function ToggleBoard({ sum, setSum }) {
                     </>
                 );
             })}
+            {pieChart.length > 0 && (
+                <ResponsiveContainer width="100%" height="40%">
+                    <PieChart width={40} height={40}>
+                        <Pie
+                            dataKey="value"
+                            isAnimationActive={false}
+                            data={pieChart}
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={80}
+                            fill="#8884d8"
+                            label
+                        />
+                        <Tooltip />
+                    </PieChart>
+                </ResponsiveContainer>
+            )}
         </>
     );
 }
