78b136b34f152b19fca8db7de64e4a702c07070a
Phu (Jack) Nguyen <phu.h.g.nguyen@gmail.com>
2022-01-29 19:26:55 -0600

    Created mock summary board

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import SumCount from "./components/sumCount";
 import ToggleBoard from "./components/toggleboard/toggleBoard";
+import SummaryBoard from "./components/summaryBoard/summaryBoard";
 import "./App.css";
 
 function App() {
@@ -17,22 +16,24 @@ function App() {
                 <Grid container spacing={2}>
                     <Grid item md={3}>
                         <div className="grid-comp">
-                            <ToggleBoard sum={sum} setSum={setSum} />
-                            <SumCount sum={sum} />
+                            <div className="toggle-board">
+                                <ToggleBoard sum={sum} setSum={setSum} />
+                                <SumCount sum={sum} />
+                            </div>
                         </div>
                     </Grid>
 
                     <Grid item md={6}>
                         <div className="grid-comp">
-                            <ToggleBoard sum={sum} setSum={setSum} />
-                            <SumCount sum={sum} />
+                            <h1>Big Boi Graphs goes here</h1>
                         </div>
                     </Grid>
 
                     <Grid item md={3}>
                         <div className="grid-comp">
-                            <ToggleBoard sum={sum} setSum={setSum} />
-                            <SumCount sum={sum} />
+                            <div className="summary">
+                                <SummaryBoard />
+                            </div>
                         </div>
                     </Grid>
                 </Grid>
